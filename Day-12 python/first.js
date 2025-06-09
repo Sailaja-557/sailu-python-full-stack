dd34eb5e79c05a63ba696fbbd36c2770e58a3c98
Sailaja-557 <22h41a0557sailaja@gmail.com>
2025-06-09 21:27:08 +0530

    code testing

---
+++ Day-12 python/first.js
@@ -0,0 +1,136 @@
+// let x = 10
+// console.log(x)
+
+
+// var n = "boom"
+// var n = "Raa"
+// console.log(n) // Raa
+
+// let age =25
+// // let age =30 ... ela esthe error vasthundi
+//  age =30
+// console.log(age)
+
+// const country = "India";
+// // country = "USA";  Error
+
+// let a = 10, b = 5;
+// console.log(a + b); // 15
+// console.log(a % b); // 0
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+
+// let p= 10
+// let s = 20
+// p += 50
+// console.log(p,s)
+
+// var c = 50
+// let k = 10
+// c -= 10
+// console.log(c)
+
+// var b = 10
+// var g = 5 
+// console.log(b * g)
+
+
+// console.log(458 == "458"); // true (check value only)
+// console.log(458 === "458"); // false (check value and checks type too ) j.s lo "===" ni use chestham Comparison ki
+
+// let boomraa= true
+// let chillraa = false
+// if (boomraa&&chillraa) {
+//     console.log("welcome brooo...")
+// }
+
+// let oyy= true
+// let arey = false
+// if (oyy||arey) {
+//     console.log("welcome brooo...")
+// }
+
+
+// let m = 10
+
+// if (m<0)
+// {
+//     console.log("given number is negative")
+// }
+// else if (m>0)
+// {
+//     console.log("given number is positive")
+// }
+// else {
+//     console.log("give number is zero")
+// }
+
+ 
+// for (let i=1; i<11; i++){
+//     console.log(i)
+// }
+
+// for (let s=2; s<21; s += 2)
+// {
+//     console.log(s)
+// }
+    
+// let i = 1;
+// while (i<=10){
+//     if(i%2===0)
+//         {
+//             console.log("even numbers are:",i);
+//         }
+//         i++;
+// }
+
+// let x = 6;
+// do {
+//   console.log(x);
+//   x++;
+// } while (x < 6); // Will run once even though condition is false
+
+
+// let user = { name: "sailu", age: 20 , clgname: "bvc" ,rollno: 557, branch:"CSE"};
+// for (let key in user) {
+//   console.log(key, user[key]);
+// }
+
+// let nums = [10, 20, 30];
+// for (let num in nums) {
+//   console.log(num);  // 0 1 2
+// }
+
+// let nums = [10, 20, 30];
+// for (let num of nums) {
+//   console.log(num); // 10 20 30
+// }
+
+// let colors = ["red", "green", "blue"];
+// console.log(colors[0]); // red
+// colors[1] = "yellow";
+
+//let arr = [1, 2, 3];
+// // console.log(arr.length); // 3
+
+// for(let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for (let item of arr) {
+//   console.log(item);
+// }
+
+// arr.forEach(function(item) {
+//   console.log(item);
+// });
+
+// for (let index in arr) {
+//   console.log(arr[index]);
+// }
+
+let arr = [1, 2, 3, 4];
+arr.push(5); // [1, 2, 3, 4, 5]
+arr.pop();   // [1, 2, 3, 4]
+console.log(arr)
